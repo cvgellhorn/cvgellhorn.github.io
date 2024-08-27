0123a7e013f5b136d76da778bfff7fa8ef5461b6
Christoph von Gellhorn <gellborn@gmail.com>
2024-08-27 22:01:37 +1000

    chore: update footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-micro.vercel.app",
-  integrations: [tailwind(), sitemap(), mdx(), pagefind()],
+  integrations: [tailwind(), icon(), sitemap(), mdx(), pagefind()],
   markdown: {
     shikiConfig: {
       theme: "css-variables",
