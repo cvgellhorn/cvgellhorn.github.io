9c1aa162acdc95452fdd85747aeaef8cfe2c1a75
Christoph von Gellhorn <gellborn@gmail.com>
2024-08-30 13:20:32 +1000

    chore: remove pagefind

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
-import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   site: "https://cvgellhorn.com.au",
-  integrations: [tailwind(), icon(), sitemap(), mdx(), pagefind()],
+  integrations: [tailwind(), icon(), sitemap(), mdx()],
   build: {
     format: "file",
   },
